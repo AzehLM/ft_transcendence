aa671968ee2dabd509e31386ecab2feffd490579
Guillaume Ebersohl <gueberso@student.42lyon.fr>
2026-03-18 15:36:45 +0100

    fix: File models updated to match models of other services

--- backend/internal/files/model.go
+++ backend/internal/files/model.go
@@ -1,22 +1,22 @@
 package files
 
 import (
-	"time"
 	"github.com/google/uuid"
+	"time"
 )
 
 type File struct {
-    ID             uuid.UUID  `gorm:"type:uuid;primaryKey"`
-    OwnerUserID    uuid.UUID  `gorm:"type:uuid;not null"`
-    OrgID          *uuid.UUID `gorm:"type:uuid"`
-    FolderID       *uuid.UUID `gorm:"type:uuid"`
-    Name           string     `gorm:"type:varchar(100);not null"`
-    FileSize       int64      `gorm:"not null"`
-    MinioObjectKey uuid.UUID  `gorm:"type:uuid;uniqueIndex;not null"`
-    EncryptedDEK   []byte     `gorm:"type:bytea;not null"`
-    IV             []byte     `gorm:"type:bytea;not null"`
-    Status         string     `gorm:"type:varchar(20);not null;default:'PENDING'"`
-    CreatedAt      time.Time
+    ID             uuid.UUID    `gorm:"type:uuid;primaryKey" json:"id"`
+    OwnerUserID    uuid.UUID    `gorm:"type:uuid;not null" json:"owner_user_id"`
+    OrgID          *uuid.UUID   `gorm:"type:uuid" json:"org_id"`
+    FolderID       *uuid.UUID   `gorm:"type:uuid" json:"folder_id"`
+    Name           string       `gorm:"size:100;not null" json:"name"`
+    FileSize       int64        `gorm:"not null" json:"file_size"`
+    MinioObjectKey uuid.UUID    `gorm:"type:uuid;uniqueIndex;not null" json:"minio_object_key"`
+    EncryptedDEK   []byte       `gorm:"type:bytea;not null" json:"encrypted_dek"`
+    IV             []byte       `gorm:"type:bytea;not null" json:"iv"`
+    Status         string       `gorm:"size:20;not null;default:'PENDING'" json:"status"`
+    CreatedAt      time.Time    `gorm:"type:timestamptz;not null;default:CURRENT_TIMESTAMP" json:"created_at"`
 }
 
 // unnecessary as GORM looks for a table as its plural in snake_case of the struct.
