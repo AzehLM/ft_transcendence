d9beb8737fe79d2cd2d082a4c946c28a52ce32fb
AzehLM <guigui1991@live.fr>
2026-03-21 13:23:08 +0100

    feat(test): updating File informations in DB

--- backend/internal/files/repository.go
+++ backend/internal/files/repository.go
@@ -38,6 +38,7 @@ func NewFileRepository(db *gorm.DB) FileRepository {
 
 // pointeur vers File et pas référence pour que GORM puisse le modifier directement en mémoire (remplir les champs)
 // pas d'exceptions en go alors .Error extrait l'erreur si il y a (nil si tout va bien)
+// this function creates a file, we do that when a user asks for a presigned URL to reserve created object ID
 func (r *fileRepository) InsertPendingFile(file *File) error {
 	return r.db.Create(file).Error
 }
@@ -45,7 +46,7 @@ func (r *fileRepository) InsertPendingFile(file *File) error {
 // Looks for the First element that has this ID as we should never have 2 same minio_object_key
 func (r *fileRepository) FindByObjectID (objectID uuid.UUID) (*File, error) {
 	var file File
-	err := r.db.Where("minio_object_key = ? status = ?", objectID, "PENDING").First(&file).Error
+	err := r.db.Where("minio_object_key = ? AND status = ?", objectID, "PENDING").First(&file).Error
 	if err != nil {
 		return nil, err
 	}
@@ -53,7 +54,17 @@ func (r *fileRepository) FindByObjectID (objectID uuid.UUID) (*File, error) {
 }
 
 
-// func (r *fileRepository) ActivateFile(objectID uuid.UUID, name string, encryptedDEK []byte, iv []byte, orgID *uuid.UUID) error {
-// 	err := r.db.
-// 	return err, nil
-// }
+// to check, zero-value are ignored by GORM in a struct
+// Which means I'll have to test for name = "", iv = nil, encryptedDEK empty etc...
+func (r *fileRepository) ActivateFile(objectID uuid.UUID, name string, encryptedDEK []byte, iv []byte, orgID *uuid.UUID) error {
+	err := r.db.Model(&File{}).
+		Where("minio_object_key = ?", objectID).
+		Updates(File{
+			Name: name,
+			EncryptedDEK: encryptedDEK,
+			IV: iv,
+			Status: "ACTIVE",
+			OrgID: orgID,
+		}).Error
+	return err
+}
