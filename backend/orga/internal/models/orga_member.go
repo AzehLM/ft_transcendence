215f4cf2b8b8a9c3f1a5fed0c598014ce9d19091
Noalune12 <louanne.buisson@gmail.com>
2026-03-23 13:27:01 +0100

    feat: add model for org_members

---
+++ backend/orga/internal/models/orga_member.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+	"github.com/google/uuid"
+)
+
+type OrgaMember struct {
+	OrgaID  			uuid.UUID `gorm:"type:uuid;primaryKey;not null;foreignKey:OrgaID;references:ID;constraint:OnDelete:CASCADE" json:"org_id"`
+	UserID				uuid.UUID `gorm:"type:uuid;primaryKey;not null;foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE" json:"user_id"`
+    Role				string    `gorm:"size:20;not null;check:role IN ('owner','admin','member')" json:"role"`
+    EncOrgaPrivateKey	[]byte    `gorm:"type:bytea;not null" json:"enc_org_priv_key"`
+    JoinedAt			time.Time `gorm:"not null;default:CURRENT_TIMESTAMP" json:"joined_at"`
+}
+
+func(OrgaMember) TableName() string {
+	return "org_members"
+}
