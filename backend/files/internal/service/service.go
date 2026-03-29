5c8f51b0bb8708d0f8bef918b02aa267c04807c6
Guillaume Ebersohl <gueberso@student.42lyon.fr>
2026-03-29 17:49:28 +0200

    feat(files service): new RequestUploadURL and FinalizeUpload methods in FileService

--- backend/files/internal/service/service.go
+++ backend/files/internal/service/service.go
@@ -2,7 +2,8 @@ package service
 
 import (
 	"context"
-	"net/url"
+	"fmt"
+	// "log"
 	"time"
 
 	files "backend/files/internal"
@@ -10,12 +11,16 @@ import (
 	"github.com/google/uuid"
 	"github.com/minio/minio-go/v7"
 	"github.com/redis/go-redis/v9"
+	"gorm.io/gorm"
 )
+
+var ErrForbidden = fmt.Errorf("forbidden")
+var ErrNotFound = fmt.Errorf("not found")
 
 // contract pour la logique métier
 type FileService interface {
 	RequestUploadURL(userID uuid.UUID, fileSize int64, folderID *uuid.UUID, orgID *uuid.UUID) (presignedURL string, objectID uuid.UUID, err error)
-	// FinalizeUpload(userID uuid.UUID, objectID uuid.UUID, name string, encryptedDEK []byte, iv []byte, orgID *uuid.UUID) error
+	FinalizeUpload(userID uuid.UUID, objectID uuid.UUID, name string, encryptedDEK []byte, iv []byte, orgID *uuid.UUID) error
 	// DownloadFile(userID uuid.UUID, fileID uuid.UUID) (presignedURL string, encryptedDEK []byte, iv []byte, name string, err error)
 	// DeleteFile(userID uuid.UUID, fileID uuid.UUID) error
 	// MoveFile(userID uuid.UUID, fileID uuid.UUID, folderID *uuid.UUID) error
@@ -50,17 +55,64 @@ RESPONSE 200
 */
 func (s *fileService) RequestUploadURL(userID uuid.UUID, fileSize int64, folderID *uuid.UUID, orgID *uuid.UUID) (presignedURL string, objectID uuid.UUID, err error) {
 
-	objectID := uuid.New()
+    ctx := context.Background()
+
+	objectID = uuid.New()
 
 	// quota verification, a voir avec pierrick
+	// if used_space + fileSize > max_space = return 413
 
-	temp := &files.File{
-		ID:	uuid.New(),
-		// ...
+	newFile := &files.File{
+		ID:				uuid.New(),
+		OwnerUserID:	userID,
+		OrgID:			orgID,
+		FolderID:		folderID,
+		Name:			"",
+		FileSize:		fileSize,
+		MinioObjectKey:	objectID,
+		EncryptedDEK:	[]byte{},
+		IV:				[]byte{},
+		Status:			"PENDING",
 	}
 
-	s.repo.InsertPendingFile(temp)
+	if err = s.repo.InsertPendingFile(newFile); err != nil {
+		return "", uuid.Nil, err
+	}
 
-	s.minioClient.PresignedPutObject(ctx context.Context, bucketName string, objectName string, expires time.Duration)
-	// return
+	// default ctx for now, not sure it will change, ostrom as the root repository, will have to be modified depending on users path
+	rawURL, err := s.minioClient.PresignedPutObject(ctx, "ostrom", objectID.String(), 15*time.Minute)
+	if err != nil {
+		return "", uuid.Nil, err
+	}
+	// log.Printf("[LOG] rawURL: %s\n", rawURL)
+	presignedURL = rawURL.String()
+	// log.Printf("[LOG] presignedURL: %s\n", presignedURL)
+
+	return presignedURL, objectID, err
+}
+
+
+func (s *fileService) FinalizeUpload(userID uuid.UUID, objectID uuid.UUID, name string, encryptedDEK []byte, iv []byte, orgID *uuid.UUID) error {
+
+	// verifier pending status
+	file, err := s.repo.FindByObjectID(objectID)
+	if err == gorm.ErrRecordNotFound {
+		return ErrNotFound
+	} else if err != nil {
+		return err
+	}
+
+	if file.OwnerUserID != userID {
+		return ErrForbidden // for future error code
+	}
+
+	// activer ficher en db
+	if err := s.repo.ActivateFile(objectID, name, encryptedDEK, iv, orgID); err != nil {
+		return err
+	}
+
+	// increment used_space -> later
+	// publier l'event file_uploaded sur redis -> later
+
+	return nil // tout c'est bien passé
 }
