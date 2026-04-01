b24687f7837482f44ec331acfb8cf5cfe1af0969
Guillaume Ebersohl <gueberso@student.42lyon.fr>
2026-04-01 21:37:31 +0200

    refactor: utilitary function to extractUserID from JWT

--- backend/storage/internal/handlers/handler.go
+++ backend/storage/internal/handlers/handler.go
@@ -1,7 +1,12 @@
 package handlers
 
 import (
+	"fmt"
+
 	"backend/storage/internal/service"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/google/uuid"
 )
 
 type StorageHandler struct {
@@ -14,14 +19,21 @@ func NewStorageHandler (svc service.StorageService) *StorageHandler {
 	}
 }
 
-type uploadURLRequest struct {
-
-}
-
 type finalizeRequest struct {
 
 }
 
-func (h *StorageHandler) extractUserID
+func (h *StorageHandler) extractUserID(c fiber.Ctx) (uuid.UUID, error) {
 
-func (svc *StorageHandler)
+	userIDstr, ok := c.Locals("user_id").(string)
+	if !ok || userIDstr == "" {
+		return uuid.Nil, fmt.Errorf("user_id missing or invalid")
+	}
+
+	userID, err := uuid.Parse(userIDstr)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("user_id is not a valid UUID")
+	}
+
+	return userID, nil
+}
