e9649941bb0f725430dfdf6ea7988be4b13becb2
Noalune12 <louanne.buisson@gmail.com>
2026-04-29 14:56:49 +0200

    feat(front): lay out of profile page(/profile)

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getPrivateKeyFromSession } from "../../services/crypto.service";
 import styles from "../../styles/profile.module.css";
 import { SettingsLayout } from "./SettingsLayout";
+import avatar from './assets/temp-avatar.png';
 
 export default function ProfilePage() {
   const [privateKey, setPrivateKey] = useState<string | null>(null);
@@ -15,12 +16,54 @@ useEffect(() => {
     setPrivateKey(base64);
   };
   init();
+
 }, []);
+const [name, setName] = useState<string>("Jean");
+const [sirname, setSirname] = useState<string>("Dupont");
+const [email, setEmail] = useState<string>("jeannot@gmail.com");
 
     return (
 
         <SettingsLayout>
-          <h2 className={styles.subtitle}>Personnal information</h2>
+          <div className={styles.mainBox}>
+            <h2 className={styles.subtitle}>Personnal information</h2>
+            <div className={styles.profileBox}>
+              <div className={styles.profileAvatarCol}>
+                <img src={avatar} alt="Avatar" />
+                <button className={`${styles.buttonChange} ${styles.profileButton}`}>Change Avatar</button>
+              </div>
+              <div className={styles.infoBox}>
+                <div className={styles.nameBox}>
+                  <div className={styles.inputBox}>
+                    <p>First Name</p>
+                    <input
+                      type="text"
+                      value={name}
+                      // onChange={(e) => setName(e.target.value)}
+                    />
+                  </div>
+                  <div className={styles.inputBox}>
+                    <p>Last Name</p>
+                    <input
+                      type="text"
+                      value={sirname}
+                    />
+                  </div>
+                </div>
+                <div className={styles.inputBox}>
+                  <p>Email</p>
+                  <input
+                    type="text"
+                    value={email}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.buttons}>
+            <button className={`${styles.buttonChange} ${styles.profileButton}`}>Save Changes</button>
+            <button className={`${styles.buttonCancel} ${styles.profileButton}`}>Cancel</button>
+          </div>
           {/* <div style={{ wordBreak: "break-all", fontSize: "12px" }}>
             {privateKey ? `-----BEGIN PRIVATE KEY-----\n${privateKey}\n-----END PRIVATE KEY-----` : "Key not found"}
           </div> */}
