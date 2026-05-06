529556137c1eb4d7512f1f81251b45702c7208dc
Guillaume Ebersohl <gueberso@student.42lyon.fr>
2026-05-06 21:47:17 +0200

    fix(css): flex: 1 on pages what needs the footer to be placed in the root css

--- frontend/src/pages/About/About.tsx
+++ frontend/src/pages/About/About.tsx
@@ -1,11 +1,16 @@
+import authStyles from "../../styles/auth.module.css";
+
 function AboutPage() {
 	return (
-		<>
+		<div
+			className={authStyles.login_page_wrapper}
+			style={{ background: "linear-gradient(to bottom right, #fef9f7, white)", alignItems: "flex-start", paddingTop: 64, display: "flex", flex: 1, minHeight: 0 }}
+		>
 			<div>
 				<h1>Ostrom</h1>
 				<p>aboutpage</p>
 			</div>
-		</>
+		</div>
 	)
 }
 
