1f3f21a86a2cc693100c8b0b933bb45df55ea71b
Victoire PERRIER <134929082+VictoirePrr@users.noreply.github.com>
2026-05-07 16:25:18 +0200

    feat(totp): enhance TOTP service with recovery code generation and random string utility

--- backend/auth/internal/service/totp_service.go
+++ backend/auth/internal/service/totp_service.go
@@ -1,7 +1,12 @@
 package service
 
 import (
+
+	"crypto/rand"
+    "fmt"
+	"encoding/json" 
 	"github.com/pquerna/otp/totp"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type TOTPService struct {
@@ -25,13 +30,55 @@ func (s *TOTPService) GenerateTOTPSecret(userEmail string) (string, string, erro
 
 func (s *TOTPService) VerifyTOTPCode(secret, userCode string) bool {
 
+	return totp.Validate(userCode, secret)
+
 }
 
 func (s *TOTPService) GenerateRecoveryCodes(count int) []string {
 
+    codes := make([]string, count)
+    letters := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    digits := "0123456789"
+
+    for i := 0; i < count; i++ {
+        part1 := s.randomString(3, letters)
+        part2 := s.randomString(3, digits)
+        part3 := s.randomString(3, letters)
+
+        codes[i] = fmt.Sprintf("%s-%s-%s", part1, part2, part3)
+    }
+
+    return codes
 }
 
 
-func (s *TOTPService) HashRecoveryCodes(codes []string) []byte {
+func (s *TOTPService) randomString(length int, charset string) string {
+    result := ""
+    randomByte := make([]byte, length)
+    rand.Read(randomByte)
+    
+    for i := 0; i < length; i++ {
+        randomIndex := int(randomByte[i]) % len(charset)
+        result += string(charset[randomIndex])
+    }
+    return result
+}
+
+
+
+
+func (s *TOTPService) HashRecoveryCodes(codes []string) ([]byte, error) {
+
+	hashedCodes := make([]string, len(codes))
+    
+    for i, code := range codes {
+        hash, err := bcrypt.GenerateFromPassword([]byte(code), bcrypt.DefaultCost)
+        if err != nil {
+            return nil, err
+        }
+        hashedCodes[i] = string(hash)
+    }
+    
+    return json.Marshal(hashedCodes)
 
 } 
