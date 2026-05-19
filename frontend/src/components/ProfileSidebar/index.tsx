79d853780e72eaeff37a5c3a8c903060c204fd36
Noalune12 <louanne.buisson@gmail.com>
2026-05-19 07:51:40 +0200

    fix(front): change url of storage to avoid redirection to cloudfare when refresh

--- frontend/src/components/ProfileSidebar/index.tsx
+++ frontend/src/components/ProfileSidebar/index.tsx
@@ -7,7 +7,7 @@ export function ProfileSidebar() {
   return (
     <Sidebar>
       <SidebarLink to="/profile"       icon={<User />}    label="Profile" />
-      <SidebarLink to="/storage"       icon={<HardDrive />} label="Storage" />
+      <SidebarLink to="/usage"       icon={<HardDrive />} label="Storage" />
       <SidebarLink to="/account"       icon={<Shield />}  label="Account" />
       <SidebarLink to="/organizations" icon={<Network />} label="Organizations" />
     </Sidebar>
