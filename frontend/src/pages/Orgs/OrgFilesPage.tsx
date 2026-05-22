b9e32aa815566dd5d690b815b3c971ddd3ca1e8f
Noalune12 <louanne.buisson@gmail.com>
2026-05-22 12:37:04 +0200

    feat(front): new diplay of org file page with create and upload buttons working + folder display

--- frontend/src/pages/Orgs/OrgFilesPage.tsx
+++ frontend/src/pages/Orgs/OrgFilesPage.tsx
@@ -8,6 +8,12 @@ import { useE2EEUpload } from "../../hooks/useE2EEUpload";
 import { useE2EEDownloadOrg } from "../../hooks/useE2EEDownloadOrg";
 import styles from "../Dashboard/Dashboard.module.css";
 import { OrgLayout } from "./OrgLayout";
+import { Breadcrumb } from "../../components/Breadcrumb";
+import { UploadStatus } from "../../components/UploadStatus.tsx";
+import { FolderCard } from "../../components/FolderCard";
+import { FileCard } from "../../components/FileCard"
+import { ConfirmationModal } from "../../components/ConfirmationModal";
+import { ActionButtons } from "../../components/ActionButtons";
 
 export default function OrgFilesPage() {
   const { id } = useParams();
@@ -75,8 +81,9 @@ export default function OrgFilesPage() {
   }, id, folderId);
   
   const activeUploads = Object.values(uploads);
+  const isUploading = activeUploads.some(u => u.isUploading);
 
-  const { downloadAndDecryptOrg, downloadStatus } = useE2EEDownloadOrg();
+  const { downloadAndDecryptOrg, downloadStatus, isDownloading } = useE2EEDownloadOrg();
 
   const handleDownload = (fileId: string) => {
     downloadAndDecryptOrg(fileId, id!);
@@ -176,10 +183,98 @@ export default function OrgFilesPage() {
       }
     };
 
+  const [isFolderModalOpen, setIsFolderModalOpen] = useState(false);
+  const [folderName, setFolderName] = useState("");
+  const [folderError, setFolderError] = useState<string | null>(null);
+
+  const handleCreateFolderSubmit = async () => {
+        setSuccess("");
+        setError(null);
+    if (!folderName.trim()) {
+        setFolderError("Invalid Name")
+        return;
+    }
+    try {
+      await FilesService.createFolder(folderName, folderId, id);
+      await loadFiles(folderId);
+      setSuccess("Folder created");
+      setFolderName("");
+      setIsFolderModalOpen(false);
+      setFolderError(null);
+    } catch (err: any) {
+      setFolderError(err.message || "Failed to create folder.");
+    }
+  };
+
+
   return (
     <>
     <OrgLayout orgName={orgName} orgDesc={orgDesc}>
-      <div className={styles.uploadContainer} style={{ margin: "20px 5%", marginTop: 0 }}>
+      <ConfirmationModal
+      isOpen={isFolderModalOpen}
+      fileName={folderName}
+      onConfirm={handleCreateFolderSubmit}
+      onCancel={() => { setIsFolderModalOpen(false); setFolderError(null); }}
+      isCreateFolder={true}
+      inputValue={folderName}
+      onInputChange={setFolderName}
+      errorMessage={folderError ?? undefined}
+      />
+
+      <ActionButtons onUploadFile={uploadFile}
+                      onCreateFolder={() => setIsFolderModalOpen(true)}
+        />
+
+      {loading ? (
+          <p>Loading files...</p>
+          ) : files.length === 0 && folders.length === 0 ? (
+          <p className={styles.noFile}>No files yet.</p>
+          ) : (
+          <div className={styles.contentsGrid} style={{ opacity: isDownloading || isUploading ? 0.5 : 1 }}>
+              {
+                  folders.length > 0 && (
+                      <div className={styles.itemsGrid}>
+                          <p className={styles.itemsTitle}>FOLDERS</p>
+                          <div className={styles.foldersGrid}>
+                              {folders.map(folder => (
+                              <FolderCard
+                                  key={folder.id}
+                                  id={folder.id}
+                                  name={folder.name}
+                                  createdAt={folder.created_at}
+                                  onDelete={handleDeleteFolder}
+                                  onRename={handleRenameFolder}
+                                  onMove={handleMoveFolder}
+                                  orgId={id}
+                              />
+                              ))}
+                          </div>
+                      </div>
+                  )
+              }
+              {/* {
+                  files.length > 0 && (
+                      <div className={styles.itemsGrid}>
+                          <p className={styles.itemsTitle}>FILES</p>
+                          <div className={styles.filesList}>
+                              {files.map(file => (
+                              <FileCard
+                                  key={file.id}
+                                  id={file.id}
+                                  name={file.name}
+                                  fileSize={file.file_size}
+                                  onDelete={handleDeleteFile}
+                                  onDownload={downloadAndDecrypt}
+                                  onMove={handleMoveFile}
+                              />
+                              ))}
+                          </div>
+                      </div>
+                  )
+              } */}
+          </div>
+      )}
+      {/* <div className={styles.uploadContainer} style={{ margin: "20px 5%", marginTop: 0 }}>
         {error && (
           <div className={`${styles.statusMessage} ${styles.error}`}>
             {error}
@@ -273,7 +368,7 @@ export default function OrgFilesPage() {
         onRename={handleRenameFolder}
         onMoveFolder={handleMoveFolder}
         onMoveFile={handleMoveFile}
-      />
+      /> */}
     </OrgLayout>
     </>
   );
