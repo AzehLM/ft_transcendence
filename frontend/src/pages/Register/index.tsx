86d5090b2d453de475508c99ab8c9947c0b7858a
Noalune12 <louanne.buisson@gmail.com>
2026-06-02 08:40:13 +0200

    refactor(front): set error message global to avoid information leakage in register

--- frontend/src/pages/Register/index.tsx
+++ frontend/src/pages/Register/index.tsx
@@ -63,8 +63,8 @@ export default function RegisterPage() {
                 if (response.status === 502 || response.status === 503) {
                     setError("Network error, please try again later.");
                 } else {
-                    const body = await response.json().catch(() => null);
-                    setError(body?.message || body?.error || "Registration failed!");
+                    // const body = await response.json().catch(() => null);
+                    setError("Registration failed!");
                 }
                 setIsLoading(false);
                 return;
